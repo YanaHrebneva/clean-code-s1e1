9b5ccb84570e86f9cf3c2fa072a5dd4488959efd
Yana Hrebneva <ygrebneva96@gmail.com>
2022-05-31 16:47:51 +0300

    refactor: adjust HTML-formatting according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className='delete__img';
+    deleteButtonImg.setAttribute('alt', '');
     deleteButton.appendChild(deleteButtonImg);
 
 
